3b89008e03e17fc06d1618949099929cbeb33a99
chedly99 <mohamed.chedliiy@gmail.com>
2022-02-03 17:49:56 +0100

    task6

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,1 +1,47 @@
-1
+interface DirectorInterface {
+    workFromHome():string;
+    getCoffeeBreak():string;
+    workDirectorTasks():string;
+}
+
+interface TeacherInterface {
+    workFromHome():string;
+    getCoffeeBreak():string;
+    workTeacherTasks():string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+
+}
+
+class Teacher implements TeacherInterface{
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+
+}
+
+const createEmployee = (salary: (number | string)): (Director | Teacher) => {
+    if(salary < 500)
+        return new Teacher();
+    return new Director();
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee(2000));
